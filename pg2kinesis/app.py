324042aa9f12b74626c543fd0da644da03120611
tringuyenminh2801 <trinm2801@gmail.com>
2024-04-18 17:37:52 +0700

    Update CDC script

--- pg2kinesis/app.py
+++ pg2kinesis/app.py
@@ -11,7 +11,7 @@ with open("config.yml", "r") as stream:
         config = yaml.safe_load(stream=stream)
     except yaml.YAMLError as yamlErr:
         print(yamlErr)
-        
+
 # SETUP CONFIGURATION
 dbConfig = config['instances']['pg']
 repluserConfig = config['pg2kinesis']['repluser']
@@ -19,9 +19,10 @@ kinesisConfig = config['pg2kinesis']['kinesis']
 
 stream_name = kinesisConfig['stream_name']
 kinesis_client = boto3.client(
-    'kinesis', 
+    'kinesis',
+    aws_access_key_id=kinesisConfig['credentials']['aws_access_key_id'],
+    aws_secret_access_key=kinesisConfig['credentials']['aws_secret_access_key'],
     region_name=kinesisConfig['region_name'])
-
 
 class Consumer:
     def __call__(self, msg):
@@ -32,8 +33,12 @@ class Consumer:
             pass
         else:
             for changeData in jsonPayload:
-                columnnames = changeData['columnnames']
-                columnvalues = changeData['columnvalues']
+                if changeData['kind'] == 'delete':
+                    columnnames = changeData['oldkeys']['keynames']
+                    columnvalues = changeData['oldkeys']['keyvalues']
+                else:
+                    columnnames = changeData['columnnames']
+                    columnvalues = changeData['columnvalues']
                 processedData = {k : v for k, v in zip(columnnames, columnvalues)}
                 processedData["event_timestamp"] = str(datetime.now())
                 processedData['kind'] = changeData['kind']
@@ -41,18 +46,14 @@ class Consumer:
                 processedData['table'] = changeData['table']
                 print(f"Data: {processedData}")
                 print("Put one record to Kinesis...\n")
-                #kinesis_client.put_record(StreamName=stream_name, Data=json.dumps(processedData), PartitionKey="default")
+                kinesis_client.put_record(StreamName=stream_name, Data=json.dumps(processedData), PartitionKey="default")
 
 def main():
     global dbConfig, repluserConfig
     # ESTABLISH SSH CONNECTION
     try:
         my_connection  = psycopg2.connect(
-                        f"dbname='{dbConfig['dbname']}' 
-                        host='{dbConfig['host']}' 
-                        port=5432 
-                        user='{repluserConfig['username']}' 
-                        password='{repluserConfig['password']}'" ,
+                        f"dbname='{dbConfig['dbname']}' host='{dbConfig['host']}' port=5432 user='{repluserConfig['username']}' password='{repluserConfig['password']}'" ,
                         connection_factory = LogicalReplicationConnection)
     except Exception as e:
         print("ERROR: Unexpected error: Could not connect to RDS for PostgreSQL instance.")
@@ -61,26 +62,22 @@ def main():
 
     print("SUCCESS: Connection to RDS for PostgreSQL instance succeeded")
     cur = my_connection.cursor()
-    options = {'publication_names': 'cdc', 'proto_version': '1'}
-    
+    options = {'pretty-print' : 1}
+
     # CREATE REPLICATION SLOTS
     try:
         cur.create_replication_slot('cdc', output_plugin = 'wal2json')
-        
+
     # IN CASE THE REPLICATION SLOT EXISTS, DROP AND CREATE NEW ONE
     except psycopg2.ProgrammingError:
         print("Replication slot exists!")
         cur.drop_replication_slot(slot_name='cdc')
         cur.create_replication_slot(slot_name='cdc', output_plugin = 'wal2json')
-        
+
     # START LISTENING TO CHANGE DATA
     cur.start_replication(slot_name='cdc', options=options, decode= True)
     consumer = Consumer()
-    try:
-        cur.consume_stream(consume=consumer)
-    except:
-        print("Stop replication...")
-            
+    cur.consume_stream(consume=consumer)
 
 if __name__ == "__main__":
     main()

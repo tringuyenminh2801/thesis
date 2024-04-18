cb1e6394ed23bc85140604585b01276c87ad9d04
tringuyenminh2801 <trinm2801@gmail.com>
2024-04-18 16:52:05 +0700

    Update CDC script

--- pg2kinesis/app.py
+++ pg2kinesis/app.py
@@ -22,30 +22,37 @@ kinesis_client = boto3.client(
     'kinesis', 
     region_name=kinesisConfig['region_name'])
 
-def consume(msg):
-    jsonPayload = json.loads(msg.payload)['change']
-    if not jsonPayload:
-        print("Nothing changed, skip this message!\n")
-        pass
-    else:
-        for changeData in jsonPayload:
-            columnnames = changeData['columnnames']
-            columnvalues = changeData['columnvalues']
-            processedData = {k : v for k, v in zip(columnnames, columnvalues)}
-            processedData["event_timestamp"] = str(datetime.now())
-            processedData['kind'] = changeData['kind']
-            processedData['schema'] = changeData['schema']
-            processedData['table'] = changeData['table']
-            print(f"Data: {processedData}")
-            print("Put one record to Kinesis...\n")
-            #kinesis_client.put_record(StreamName=stream_name, Data=json.dumps(processedData), PartitionKey="default")
+
+class Consumer:
+    def __call__(self, msg):
+        jsonPayload = json.loads(msg.payload)['change']
+        print(json.loads(msg.payload))
+        if not jsonPayload:
+            print("Nothing changed, skip this message!\n")
+            pass
+        else:
+            for changeData in jsonPayload:
+                columnnames = changeData['columnnames']
+                columnvalues = changeData['columnvalues']
+                processedData = {k : v for k, v in zip(columnnames, columnvalues)}
+                processedData["event_timestamp"] = str(datetime.now())
+                processedData['kind'] = changeData['kind']
+                processedData['schema'] = changeData['schema']
+                processedData['table'] = changeData['table']
+                print(f"Data: {processedData}")
+                print("Put one record to Kinesis...\n")
+                #kinesis_client.put_record(StreamName=stream_name, Data=json.dumps(processedData), PartitionKey="default")
 
 def main():
     global dbConfig, repluserConfig
     # ESTABLISH SSH CONNECTION
     try:
         my_connection  = psycopg2.connect(
-                        f"dbname='{dbConfig['dbname']}' host='{dbConfig['host']}' port=5432 user='{repluserConfig['username']}' password='{repluserConfig['password']}'" ,
+                        f"dbname='{dbConfig['dbname']}' 
+                        host='{dbConfig['host']}' 
+                        port=5432 
+                        user='{repluserConfig['username']}' 
+                        password='{repluserConfig['password']}'" ,
                         connection_factory = LogicalReplicationConnection)
     except Exception as e:
         print("ERROR: Unexpected error: Could not connect to RDS for PostgreSQL instance.")
@@ -54,15 +61,26 @@ def main():
 
     print("SUCCESS: Connection to RDS for PostgreSQL instance succeeded")
     cur = my_connection.cursor()
+    options = {'publication_names': 'cdc', 'proto_version': '1'}
+    
+    # CREATE REPLICATION SLOTS
     try:
         cur.create_replication_slot('cdc', output_plugin = 'wal2json')
-    except:
+        
+    # IN CASE THE REPLICATION SLOT EXISTS, DROP AND CREATE NEW ONE
+    except psycopg2.ProgrammingError:
         print("Replication slot exists!")
-        cur.drop_replication_slot('cdc')
-        cur.create_replication_slot('cdc', output_plugin = 'wal2json')
-    finally:
-        cur.start_replication(slot_name = 'cdc', options = {'pretty-print' : 1}, decode= True)
-        cur.consume_stream(consume=consume)
+        cur.drop_replication_slot(slot_name='cdc')
+        cur.create_replication_slot(slot_name='cdc', output_plugin = 'wal2json')
+        
+    # START LISTENING TO CHANGE DATA
+    cur.start_replication(slot_name='cdc', options=options, decode= True)
+    consumer = Consumer()
+    try:
+        cur.consume_stream(consume=consumer)
+    except:
+        print("Stop replication...")
+            
 
 if __name__ == "__main__":
     main()
